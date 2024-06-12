1989e5ebda07bba6b6f3d1d4b0d1bf0a2155c896
Chriton <defisummer@gmail.com>
2024-06-12 15:43:41 +0300

    add symlink for docker compose

--- install_docker.sh
+++ install_docker.sh
@@ -43,6 +43,11 @@ echo "==========================================================================
 echo "Installing Docker Compose..."
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+echo "Creating a symbolic link for Docker Compose..."
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+echo "Symbolic link created."
+
 echo "Docker Compose installation complete."
 echo "=============================================================================="
 
